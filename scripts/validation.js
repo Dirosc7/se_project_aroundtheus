6f79238a06e083a3055ede300e58972c6a15f435
Dirosc7 <125402929+Dirosc7@users.noreply.github.com>
2023-07-12 20:04:21 -0400

    Update validation.js

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,1 +1,17 @@
 console.log("hello world");
+
+function enableValidation(config) {
+  console.log(config);
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible"
+};
+
+  
+enableValidation(config); 
