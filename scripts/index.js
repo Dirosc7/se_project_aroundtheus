468a1f17cffa7360bd875a618612d17fa4490b22
Dirosc7 <125402929+Dirosc7@users.noreply.github.com>
2023-09-09 15:16:33 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
+import {openModal, closeModal, handleModalClose, handleKeyDown} from "../utils/utils.js";
 // ARRAY  //
 //        //
 const object1 = {
